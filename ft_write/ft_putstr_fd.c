8245fd35b4af8cc375a7bd312c242c47ae3acefe
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2023-12-14 15:42:00 +0100

    don't write zero terminator (fix)

--- ft_write/ft_putstr_fd.c
+++ ft_write/ft_putstr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: trusanov <trusanov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lporoshi <lporoshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:48:01 by lporoshi          #+#    #+#             */
-/*   Updated: 2023/12/11 14:31:16 by trusanov         ###   ########.fr       */
+/*   Updated: 2023/12/14 15:38:49 by lporoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ int	ft_putstr_fd(char *s, int fd)
 {
 	if (s == NULL)
 		return (write(fd, "(null)", 6));
-	return (write(fd, s, ft_strlen(s)));
+	return (write(fd, s, ft_strlen(s) - 1));
 }
