ea812ca1c22c7f9e7cf9b8c05e617bdc28d45052
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2023-12-15 17:25:16 +0100

    rework ft_surround function

--- ft_string/ft_str_surround.c
+++ ft_string/ft_str_surround.c
@@ -6,33 +6,31 @@
 /*   By: lporoshi <lporoshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 16:22:19 by lporoshi          #+#    #+#             */
-/*   Updated: 2023/12/15 16:48:58 by lporoshi         ###   ########.fr       */
+/*   Updated: 2023/12/15 17:20:18 by lporoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_str_surround(char **s, char surround_char)
+char	*ft_str_surround(char **s, char surround_char)
 {
 	char	*res;
 	int		len;
 
 	if (!s || !(*s))
-		return (FT_ERROR);
+		return (NULL);
 	len = ft_strlen(*s);
 	if (len == -1)
-		return (FT_ERROR);
-	res = ft_calloc(len + 3, sizeof(char));
+		return (NULL);
+	res = (char *)ft_calloc(len + 4, sizeof(char));
 	if (res == NULL)
-		return (FT_ERROR);
+		return (NULL);
 	res[0] = surround_char;
 	if (ft_strncpy(res + 1, *s, len + 1) == NULL)
 	{
 		free(res);
-		return (FT_ERROR);
+		return (NULL);
 	}
 	res[len + 1] = surround_char;
-	free(*s);
-	*s = res;
-	return (FT_SUCCESS);
+	return (res);
 }
