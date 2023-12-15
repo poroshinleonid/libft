7afa0e9c8aba34aabfdf6f9c4d89c806697a9c15
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2023-12-15 13:00:18 +0100

    Revert "fix putstr again, it was, indeed working properly"
    This reverts commit 78305f4eb31691d20d6791b41c7258d974618830.

--- ft_write/ft_putstr_fd.c
+++ ft_write/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: lporoshi <lporoshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:48:01 by lporoshi          #+#    #+#             */
-/*   Updated: 2023/12/14 17:19:51 by lporoshi         ###   ########.fr       */
+/*   Updated: 2023/12/14 15:38:49 by lporoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ int	ft_putstr_fd(char *s, int fd)
 {
 	if (s == NULL)
 		return (write(fd, "(null)", 6));
-	return (write(fd, s, ft_strlen(s)));
+	return (write(fd, s, ft_strlen(s) - 1));
 }
